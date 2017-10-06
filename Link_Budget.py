bc6d303b4798203de5ffe66e0bf12e5a822faf69
Shytor <Shytor@wlan-145-94-194-5.wlan.tudelft.nl>
2017-10-06 16:21:14 +0200

    use excel

--- Link_Budget.py
+++ Link_Budget.py
@@ -5,3 +5,23 @@
 ##Total Spacecraft power
 powerTotal = 100
 ##Transmitter power
+transmitP = 25
+transmitPGround = 1000
+##Loss Factors
+L_transmit = 0.8
+L_receive = 0.7
+##downlink freq in GHz
+freq = 2.2
+##uplink/downlink turn around ratio
+turnAroundRatio = [221,240] #use 221/240
+
+##Antenna specs (m)
+diameter = 0.35
+diameterGround = 5
+
+##orbit altitude
+altitude = 670000
+
+##Elongation 
+
+
